819717d1c4bd3540e0fb91e395e513f3bc53ffab
Mireles <mireles@narrowmark.com>
2015-11-12 11:36:35 -0500

    - Started adding tests. - Fixed board size logic: boards generated had spaces that were too large. - Added way to make arbitrarily large boards on initialization.

--- board.rb
+++ board.rb
@@ -2,30 +2,51 @@ class Board
   include EventDispatcher
 
   attr_accessor :board
+  attr_reader :board_size
   attr_accessor :markers
   attr_reader :victory_type
 
-  def initialize
-    @board_side = 3
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+  def initialize(size=3)
+    @board_size = size
+    @board = []
+    0.upto(@board_size ** 2 - 1) do |p|
+      @board << p.to_s
+    end
     @markers = Array.new
+    puts "#{@board}"
   end
 
   def display_board
     puts_row = ->(row) {
-      0.upto(@board_side-1) do |r|
-        print "|_#{@board[row * @board_side + r]}_"
+      0.upto(@board_size-1) do |r|
+        print "|_#{@board[row * @board_size + r]}_"
       end
         puts "|"
     }
 
-    0.upto(@board_side-1) do |c|
+    0.upto(@board_size-1) do |c|
       puts_row.call(c)
     end
   end
 
   def win?
     b = @board
+    # Does not like calls to b[row * @board_size + ]
+    # 37:in `*': nil can't be coerced into Fixnum (TypeError)
+    # Figure it out later.
+=begin
+    row_pick = []
+    get_row = ->(row) {
+      row_pick = []
+      0.upto(@board_side-1) do |r|
+        puts "#{b[row * @board_size + r]}"
+        # row_pick << b[row * @board_size + r]
+      end
+    }
+
+    test = get_row.call(0)
+    puts "#{test}"
+=end
     [b[0], b[1], b[2]].uniq.length == 1 ||
     [b[3], b[4], b[5]].uniq.length == 1 ||
     [b[6], b[7], b[8]].uniq.length == 1 ||
